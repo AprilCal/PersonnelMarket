1ab42ec542ec95c394629bd775c4b1ebee3e4587
weizige <weizige@qq.com>
2018-01-16 10:58:16 +0800

    sb

--- src/market/busi/SubmitBusi.java
+++ src/market/busi/SubmitBusi.java
@@ -1,6 +1,7 @@
 package market.busi;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import market.dao.SubmitDao;
 import market.vo.Submit;
@@ -10,9 +11,24 @@ public class SubmitBusi {
 	private SubmitDao sDao = new SubmitDao();
 	
 	//投递简历
-	public void deliver(Submit submit) throws SQLException{
+	public boolean deliver(Submit submit) throws SQLException{
 		//submit
+		int resumeId=submit.getResumeId();
+		int enterpriseId=submit.getEnterpriseId();
+		int recruitmentId=submit.getRecruitmentId();
+		List<Submit> rlist=sDao.getAllSubmitBycustomerId(resumeId);
+		 for (int i = 0; i < rlist.size(); i++) 
+		 {
+			Submit sub=rlist.get(i);
+			if((sub.getEnterpriseId()==enterpriseId)&&(sub.getRecruitmentId()==recruitmentId))
+			{
+				return false;
+			}
+	        
+	     }
+		
 		sDao.insert(submit);
+		return true;
 	}
 	
 	//接受简历
@@ -32,15 +48,22 @@ public class SubmitBusi {
 	}
 	
 	public static void main(String[] args) {
-		Submit submit = new Submit();
-		submit.setResumeId(1);
-		submit.setEnterpriseId(2);
-		submit.setRecruitmentId(3);
-		submit.setReadOrNot(true);
-		submit.setDeleted(false);
-		submit.setState("unknown");
-		submit.setCustomerId(2);
-		//SubmitBusi sBusi = new SubmitBusi();
+//		Submit submit = new Submit();
+//		submit.setResumeId(1);
+//		submit.setEnterpriseId(2);
+//		submit.setRecruitmentId(3);
+//		submit.setReadOrNot(true);
+//		submit.setDeleted(false);
+//		submit.setState("unknown");
+//		submit.setCustomerId(2);
+//		SubmitBusi sBusi = new SubmitBusi();
+//		try {
+//			boolean flag=sBusi.deliver(submit);
+//			System.out.println(flag);
+//		} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 		//sBusi.admitResume(submit);
 		//sBusi.refuseResume(submit);
 		//sDao.update(submit);
